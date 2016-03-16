98f80a36a0754d3178b84ac828d054d024046e8c
anovikov <anovikov@aif.ru>
2016-03-16 23:50:14 +0300

    15/03/2016 fix for  php version 7.x

--- Application/PHPRoll.php
+++ Application/PHPRoll.php
@@ -93,10 +93,10 @@ class PHPRoll
         $result = '';
         switch ($_SERVER['REQUEST_METHOD'])
         {
+            case 'PUT':
             case 'POST':
                 $result = $pattern(key($this->params));
                 break;
-            case 'PUT':
             case 'GET':
             case 'DELETE':
             default:
