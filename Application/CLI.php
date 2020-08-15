25db10c8ce38806f113735b5d91c90b73dea0dea
Andrey Novikov <bermud@nm.ru>
2020-08-15 20:26:07 +0300

    15/08/2020 v.2.1.2b update

--- Application/CLI.php
+++ Application/CLI.php
@@ -19,25 +19,26 @@ abstract class CLI
 {
     public $args = [];
     public $pidfile = null;
+    public $threadfile = null;
     public $cron = null;
 
     private $__running = false;
     public $processPID = null;
 
     protected static $scriptID;
-    public  $path;
-    public  $file;
+    public $path;
+    public $file;
 
     const THREAD_DEFAULT = 0;
     const THREAD_INFINITY = 1;
     const THREAD_LAZYJOB = 2;
-    const THREAD_NOTCOMPLETE = 4;
-    const THREAD_COMPLETE = 8;
+    const THREAD_EMPTY = 4;
+    const THREAD_RESULTSET = 8;
+    const THREAD_COMPLETE = 16;
 
     public $looper = true;
     public $max_threads = 0;
     public $thread_idx = 0;
-    private $thread_pid = null;
     private $threads = [];
     private $threads_timeout = 0;
 
@@ -71,6 +72,8 @@ abstract class CLI
                 // yum install -y php-process
                 if (posix_kill($this->processPID, 0)) {
                     $this->__running = true;
+                } else {
+                    @unlink($this->pidfile);
                 }
             }
 
@@ -130,8 +133,10 @@ abstract class CLI
      *
      */
     public function __destruct() {
-        if (!$this->__running && $this->pidfile && file_exists($this->pidfile)) {
-            @unlink($this->pidfile);
+        $pidfile = null;
+        if ($this->pidfile) $pidfile = $this->thread_idx == 0 ? $this->pidfile : $this->pidfile .":" . $this->thread_idx;
+        if (!$this->__running && $pidfile && file_exists($pidfile)) {
+            @unlink($pidfile);
         }
     }
 
@@ -164,13 +169,25 @@ abstract class CLI
     abstract function run();
 
     /**
-     * System signals handlers
-     *
-     * @function SIGTERM();
-     * @function SIGINT();
-     * @function SIGHUP();
+     * System signals handlers:
+     * @function SIGTERM(), ...
+     * 1) SIGHUP       2) SIGINT       3) SIGQUIT      4) SIGILL
+     * 5) SIGTRAP      6) SIGABRT      7) SIGBUS       8) SIGFPE
+     * 9) SIGKILL      10) SIGUSR1     11) SIGSEGV     12) SIGUSR2
+     * 13) SIGPIPE     14) SIGALRM     15) SIGTERM     16) SIGSTKFLT
+     * 17) SIGCHLD     18) SIGCONT     19) SIGSTOP     20) SIGTSTP
+     * 21) SIGTTIN     22) SIGTTOU     23) SIGURG      24) SIGXCPU
+     * 25) SIGXFSZ     26) SIGVTALRM   27) SIGPROF     28) SIGWINCH
+     * 29) SIGIO       30) SIGPWR      31) SIGSYS      34) SIGRTMIN
+     * 35) SIGRTMIN+1  36) SIGRTMIN+2  37) SIGRTMIN+3  38) SIGRTMIN+4
+     * 39) SIGRTMIN+5  40) SIGRTMIN+6  41) SIGRTMIN+7  42) SIGRTMIN+8
+     * 43) SIGRTMIN+9  44) SIGRTMIN+10 45) SIGRTMIN+11 46) SIGRTMIN+12
+     * 47) SIGRTMIN+13 48) SIGRTMIN+14 49) SIGRTMIN+15 50) SIGRTMAX-14
+     * 51) SIGRTMAX-13 52) SIGRTMAX-12 53) SIGRTMAX-11 54) SIGRTMAX-10
+     * 55) SIGRTMAX-9  56) SIGRTMAX-8  57) SIGRTMAX-7  58) SIGRTMAX-6
+     * 59) SIGRTMAX-5  60) SIGRTMAX-4  61) SIGRTMAX-3  62) SIGRTMAX-2
+     * 63) SIGRTMAX-1  64) SIGRTMAX
      */
-
     /**
      * @function threadin
      *
@@ -181,17 +198,27 @@ abstract class CLI
      * SIGTERM — окончание процесса. Происходит, когда процесс останавливают командой kill (либо другой командой, посылающей такой сигнал).
      * SIGHUP - Кроме остановки выполнения скрипта, существует также сигнал перезапуска. Его часто используют для обновления конфигурации работающих процессов без их остановки.
      * pkill -HUP -f test.php
+     * ps -efw | grep php | grep -v grep | awk '{print $2}' | xargs kill
+     * netstat -anop
      */
     final function threading(int $max = 5, int $opt = \Application\CLI::THREAD_DEFAULT, int $timeout=0)
     {
-        if (method_exists($this,'SIGTERM')) { pcntl_signal(SIGTERM, array($this, "SIGTERM")); }
-        if (method_exists($this,'SIGINT')) { pcntl_signal(SIGINT, array($this, "SIGINT")); }
-        if (method_exists($this,'SIGHUP')) { pcntl_signal(SIGHUP, array($this, "SIGHUP")); }
-        if (version_compare(PHP_VERSION, "5.3.0", '>=')) { pcntl_signal_dispatch(); }
+        if (version_compare(PHP_VERSION, "5.3.0", '>=')) {
+//            pcntl_signal_dispatch();
+            pcntl_async_signals(true);
+        }
+        if (method_exists($this,'SIGTERM')) { pcntl_signal(SIGTERM, [$this, "SIGTERM"]); }
+        if (method_exists($this,'SIGTSTP')) { pcntl_signal(SIGTSTP, [$this, "SIGTSTP"]); }
+        if (method_exists($this,'SIGINT')) { pcntl_signal(SIGINT, [$this, "SIGINT"]); }
+        if (method_exists($this,'SIGQUIT')) { pcntl_signal(SIGQUIT, [$this, "SIGQUIT"]); }
+        if (method_exists($this,'SIGHUP')) { pcntl_signal(SIGHUP, [$this, "SIGHUP"]); }
 
         $this->looper = true;
         $this->max_threads = $max;
         $timeout = $opt & \Application\CLI::THREAD_INFINITY ? $timeout : 0;
+        $empty_length = 0;
+        $complete = false;
+        $waite_empty_results = !($opt & \Application\CLI::THREAD_INFINITY);
 
         while ( $this->looper ) {
             while ( count($this->threads) > $this->max_threads ) {
@@ -203,26 +230,31 @@ abstract class CLI
 //                        unset($this->threads[$pid]);
 //                    }
                     $child = pcntl_wait($result);
+
                     if (pcntl_wifexited($result) !== 0) {
                         $code = pcntl_wexitstatus($result);
-                        if ($code & \Application\CLI::THREAD_COMPLETE) {
+                        $complete = $complete || $code & \Application\CLI::THREAD_COMPLETE;
+                        $empty_length = $code & \Application\CLI::THREAD_EMPTY ? $empty_length + 1 : 0;
+                        if ($waite_empty_results && $max <= $empty_length) {
                             $this->max_threads = 0;
                             $this->looper = false;
-                        } else if ($code & \Application\CLI::THREAD_NOTCOMPLETE && $this->max_threads < $max && !$timeout) {
-                            $this->max_threads++;
+                        } else if ($code & \Application\CLI::THREAD_RESULTSET && $this->max_threads < $max && !$timeout) {
+                            if (!$complete) $this->max_threads++;
                         }
                         unset($this->threads[$child]);
                     }
                 }
                 continue;
             }
-            if ($this->looper) $this->launcher($opt, $timeout);
 
-            if ( $this->max_threads == 0 && $timeout && $this->looper) {
+            if ( $this->looper && !$complete ) $this->launcher($opt, $timeout);
+
+            if ( $this->max_threads == 0 && $timeout && $this->looper ) {
                 sleep($timeout);
                 $this->max_threads = $max;
             }
         }
+
     }
 
     /**
@@ -239,7 +271,11 @@ abstract class CLI
             trigger_error('Could not launch new job, exiting', E_USER_WARNING);
             return false;
         } else if ($pid) {
-            $this->thread_pid = $pid;
+            $this->processPID = $pid;
+            if ($this->pidfile) {
+                $pidfile = $this->thread_idx == 0 ? $this->pidfile : $this->pidfile . ":" . $this->thread_idx;
+                file_put_contents($pidfile, $this->processPID);
+            }
             $this->thread_idx = $this->threads[$pid] = $this->max_threads; // храним список детей для прослушки
         } else {
             exit( $this->run() );
