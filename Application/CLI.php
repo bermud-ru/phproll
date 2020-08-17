895e45b0012446856f477decb62c6157b92476df
Andrey Novikov <bermud@nm.ru>
2020-08-17 19:41:46 +0300

    17/08/2020 v.2.1.2b update

--- Application/CLI.php
+++ Application/CLI.php
@@ -17,6 +17,26 @@ if (version_compare(PHP_VERSION, "5.3.0", '<')) { declare(ticks = 1); }
 
 abstract class CLI
 {
+    const SYSTEM_SIGNALS = [
+        'SIGHUP' => SIGHUP, 'SIGINT'=> SIGINT , 'SIGQUIT' => SIGQUIT, 'SIGILL' => SIGILL,
+        'SIGTRAP' => SIGTRAP, 'SIGABRT5' => SIGABRT, 'SIGBUS' => SIGBUS, 'SIGFPE' => SIGFPE,
+        'SIGKILL' => SIGKILL, 'SIGUSR1' => SIGUSR1, 'SISEGV' => SIGSEGV, 'SIGUSR2' => SIGUSR2,
+        'SIGPIPE' => SIGPIPE, 'SIGALRM' => SIGALRM, 'SIGTERM' => SIGTERM, 'SIGSTKFLT' => SIGSTKFLT,
+        'SIGCHLD' => SIGCHLD, 'SIGCONT' => SIGCONT, 'SIGSTOP' => SIGSTOP, 'SIGTSTP' => SIGTSTP,
+        'SIGTTIN' => SIGTTIN,  'SIGTTOU' => SIGTTOU, 'SIGURG' => SIGURG, 'SIGXCPU' => SIGXCPU,
+        'SIGXFSZ' => SIGXFSZ, 'SIGVTALRM' => SIGVTALRM, 'SIGPROF' => SIGPROF, 'SIGWINCH' => SIGWINCH,
+        'SIGIO' => SIGPOLL, 'SIGPWR' => SIGPWR, 'SIGSYS' => SIGSYS,
+//        'SIGRTMIN' => 34,
+//        'SIGRTMIN+1' => 35, 'SIGRTMIN+2' => 36, 'SIGRTMIN+3' => 37, 'SIGRTMIN+4' => 38,
+//        'SIGRTMIN+5' => 39, 'SIGRTMIN+6' => 40, 'SIGRTMIN+7' => 41, 'SIGRTMIN+8' => 42,
+//        'SIGRTMIN+9' => 43, 'SIGRTMIN+10' => 44, 'SIGRTMIN+11' => 45, 'SIGRTMIN+12' => 46,
+//        'SIGRTMIN+13' => 47, 'SIGRTMIN+14' => 48, 'SIGRTMIN+15' => 49, 'SIGRTMAX-14' => 50,
+//        'SIGRTMAX-13' => 51, 'SIGRTMAX-12' => 52, 'SIGRTMAX-11' => 53, 'SIGRTMAX-10' => 54,
+//        'SIGRTMAX-9' => 55, 'SIGRTMAX-8' => 56, 'SIGRTMAX-7' => 57, 'SIGRTMAX-6' => 58,
+//        'SIGRTMAX-5' => 59, 'SIGRTMAX-4' => 60, 'SIGRTMAX-3' => 61, 'SIGRTMAX-2' => 62,
+//        'SIGRTMAX-1' => 63, 'SIGRTMAX' => 64,
+    ];
+
     public $args = [];
     public $pidfile = null;
     public $threadfile = null;
@@ -29,18 +49,17 @@ abstract class CLI
     public $path;
     public $file;
 
-    const THREAD_DEFAULT = 0;
-    const THREAD_INFINITY = 1;
-    const THREAD_LAZYJOB = 2;
-    const THREAD_EMPTY = 4;
-    const THREAD_RESULTSET = 8;
-    const THREAD_COMPLETE = 16;
+    const FORK_DEFAULT = 0;
+    const FORK_INFINITY = 1;
+    const FORK_LAZYJOB = 2;
+    const FORK_EMPTY = 4;
+    const FORK_RESULTSET = 8;
+    const FORK_COMPLETE = 16;
 
     public $looper = true;
-    public $max_threads = 0;
-    public $thread_idx = 0;
-    private $threads = [];
-    private $threads_timeout = 0;
+    public $max_forks = 0;
+    public $fork_idx = 0;
+    private $forks = [];
 
     /**
      * Конструктор
@@ -134,7 +153,7 @@ abstract class CLI
      */
     public function __destruct() {
         $pidfile = null;
-        if ($this->pidfile) $pidfile = $this->thread_idx == 0 ? $this->pidfile : $this->pidfile .":" . $this->thread_idx;
+        if ($this->pidfile) $pidfile = $this->fork_idx == 0 ? $this->pidfile : $this->pidfile .":" . $this->fork_idx;
         if (!$this->__running && $pidfile && file_exists($pidfile)) {
             @unlink($pidfile);
         }
@@ -169,31 +188,11 @@ abstract class CLI
     abstract function run();
 
     /**
-     * System signals handlers:
-     * @function SIGTERM(), ...
-     * 1) SIGHUP       2) SIGINT       3) SIGQUIT      4) SIGILL
-     * 5) SIGTRAP      6) SIGABRT      7) SIGBUS       8) SIGFPE
-     * 9) SIGKILL      10) SIGUSR1     11) SIGSEGV     12) SIGUSR2
-     * 13) SIGPIPE     14) SIGALRM     15) SIGTERM     16) SIGSTKFLT
-     * 17) SIGCHLD     18) SIGCONT     19) SIGSTOP     20) SIGTSTP
-     * 21) SIGTTIN     22) SIGTTOU     23) SIGURG      24) SIGXCPU
-     * 25) SIGXFSZ     26) SIGVTALRM   27) SIGPROF     28) SIGWINCH
-     * 29) SIGIO       30) SIGPWR      31) SIGSYS      34) SIGRTMIN
-     * 35) SIGRTMIN+1  36) SIGRTMIN+2  37) SIGRTMIN+3  38) SIGRTMIN+4
-     * 39) SIGRTMIN+5  40) SIGRTMIN+6  41) SIGRTMIN+7  42) SIGRTMIN+8
-     * 43) SIGRTMIN+9  44) SIGRTMIN+10 45) SIGRTMIN+11 46) SIGRTMIN+12
-     * 47) SIGRTMIN+13 48) SIGRTMIN+14 49) SIGRTMIN+15 50) SIGRTMAX-14
-     * 51) SIGRTMAX-13 52) SIGRTMAX-12 53) SIGRTMAX-11 54) SIGRTMAX-10
-     * 55) SIGRTMAX-9  56) SIGRTMAX-8  57) SIGRTMAX-7  58) SIGRTMAX-6
-     * 59) SIGRTMAX-5  60) SIGRTMAX-4  61) SIGRTMAX-3  62) SIGRTMAX-2
-     * 63) SIGRTMAX-1  64) SIGRTMAX
-     */
-    /**
-     * @function threadin
+     * @function fork
      *
-     * @param int $max threads count
+     * @param int $max forks count
      * @param int $opt
-     * @param int $timeout between threads chunk
+     * @param int $timeout between forks chunk
      * SIGINT — прерывание процесса. Случается, когда пользователь оканчивает выполнение скрипта командой "ctrl+c".
      * SIGTERM — окончание процесса. Происходит, когда процесс останавливают командой kill (либо другой командой, посылающей такой сигнал).
      * SIGHUP - Кроме остановки выполнения скрипта, существует также сигнал перезапуска. Его часто используют для обновления конфигурации работающих процессов без их остановки.
@@ -201,47 +200,46 @@ abstract class CLI
      * ps -efw | grep php | grep -v grep | awk '{print $2}' | xargs kill
      * netstat -anop
      */
-    final function threading(int $max = 5, int $opt = \Application\CLI::THREAD_DEFAULT, int $timeout=0)
+    final function fork(int $max = 5, int $opt = \Application\CLI::FORK_DEFAULT, int $timeout=0)
     {
         if (version_compare(PHP_VERSION, "5.3.0", '>=')) {
 //            pcntl_signal_dispatch();
             pcntl_async_signals(true);
         }
-        if (method_exists($this,'SIGTERM')) { pcntl_signal(SIGTERM, [$this, "SIGTERM"]); }
-        if (method_exists($this,'SIGTSTP')) { pcntl_signal(SIGTSTP, [$this, "SIGTSTP"]); }
-        if (method_exists($this,'SIGINT')) { pcntl_signal(SIGINT, [$this, "SIGINT"]); }
-        if (method_exists($this,'SIGQUIT')) { pcntl_signal(SIGQUIT, [$this, "SIGQUIT"]); }
-        if (method_exists($this,'SIGHUP')) { pcntl_signal(SIGHUP, [$this, "SIGHUP"]); }
+
+        foreach (\Application\CLI::SYSTEM_SIGNALS as $SIGNAL => $code) {
+            if (method_exists($this, $SIGNAL)) { pcntl_signal($code, [$this, $SIGNAL]); }
+        }
 
         $this->looper = true;
-        $this->max_threads = $max;
-        $timeout = $opt & \Application\CLI::THREAD_INFINITY ? $timeout : 0;
+        $this->max_forks = $max;
+        $timeout = $opt & \Application\CLI::FORK_INFINITY ? $timeout : 0;
         $empty_length = 0;
         $complete = false;
-        $waite_empty_results = !($opt & \Application\CLI::THREAD_INFINITY);
+        $waite_empty_results = !($opt & \Application\CLI::FORK_INFINITY);
 
         while ( $this->looper ) {
-            while ( count($this->threads) > $this->max_threads ) {
-                foreach ( $this->threads as $pid => $thread_num ) {
+            while ( count($this->forks) > $this->max_forks ) {
+                foreach ( $this->forks as $pid => $FORK_num ) {
 //                    $child = pcntl_waitpid($pid, $result, WNOHANG|WUNTRACED); // слушаем статус детей
 //                    if ( $child == -1 || $child > 0 ) {
 //                        $code = pcntl_wexitstatus($result);
-//                        echo "=== STATUS: [ $code ] === $child : {$this->threads[$child]} ======\n\n";
-//                        unset($this->threads[$pid]);
+//                        echo "=== STATUS: [ $code ] === $child : {$this->forks[$child]} ======\n\n";
+//                        unset($this->forks[$pid]);
 //                    }
                     $child = pcntl_wait($result);
 
                     if (pcntl_wifexited($result) !== 0) {
                         $code = pcntl_wexitstatus($result);
-                        $complete = $complete || $code & \Application\CLI::THREAD_COMPLETE;
-                        $empty_length = $code & \Application\CLI::THREAD_EMPTY ? $empty_length + 1 : 0;
+                        $complete = $complete || $code & \Application\CLI::FORK_COMPLETE;
+                        $empty_length = $code & \Application\CLI::FORK_EMPTY ? $empty_length + 1 : 0;
                         if ($waite_empty_results && $max <= $empty_length) {
-                            $this->max_threads = 0;
+                            $this->max_forks = 0;
                             $this->looper = false;
-                        } else if ($code & \Application\CLI::THREAD_RESULTSET && $this->max_threads < $max && !$timeout) {
-                            if (!$complete) $this->max_threads++;
+                        } else if ($code & \Application\CLI::FORK_RESULTSET && $this->max_forks < $max && !$timeout) {
+                            if (!$complete) $this->max_forks++;
                         }
-                        unset($this->threads[$child]);
+                        unset($this->forks[$child]);
                     }
                 }
                 continue;
@@ -249,9 +247,9 @@ abstract class CLI
 
             if ( $this->looper && !$complete ) $this->launcher($opt, $timeout);
 
-            if ( $this->max_threads == 0 && $timeout && $this->looper ) {
+            if ( $this->max_forks == 0 && $timeout && $this->looper ) {
                 sleep($timeout);
-                $this->max_threads = $max;
+                $this->max_forks = $max;
             }
         }
 
@@ -264,19 +262,19 @@ abstract class CLI
      * @param $timeout
      * @return bool
      */
-    private function launcher(int $opt = \Application\CLI::THREAD_DEFAULT, $timeout) {
+    private function launcher(int $opt = \Application\CLI::FORK_DEFAULT, $timeout) {
         $pid = pcntl_fork();
-        if (!$opt & \Application\CLI::THREAD_INFINITY || $timeout > 0) $this->max_threads--;
+        if (!$opt & \Application\CLI::FORK_INFINITY || $timeout > 0) $this->max_forks--;
         if ($pid == -1) {
             trigger_error('Could not launch new job, exiting', E_USER_WARNING);
             return false;
         } else if ($pid) {
             $this->processPID = $pid;
             if ($this->pidfile) {
-                $pidfile = $this->thread_idx == 0 ? $this->pidfile : $this->pidfile . ":" . $this->thread_idx;
+                $pidfile = $this->fork_idx == 0 ? $this->pidfile : $this->pidfile . ":" . $this->fork_idx;
                 file_put_contents($pidfile, $this->processPID);
             }
-            $this->thread_idx = $this->threads[$pid] = $this->max_threads; // храним список детей для прослушки
+            $this->fork_idx = $this->forks[$pid] = $this->max_forks; // храним список детей для прослушки
         } else {
             exit( $this->run() );
         }
